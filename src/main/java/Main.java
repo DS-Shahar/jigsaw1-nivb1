9539189adc9950bf95a55aa111579c45ad70e6e0
nivb1 <nivbarzilay2103@gmail.com>
2024-12-23 16:29:15 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,34 @@
 class Main {
+  public static Node<Integer> f(Node<Integer> list){
+		Node <Integer> newlist = new Node<Integer>(list.getNext().getValue());
+		Node <Integer> n = new Node<Integer>(-1);
+		Node <Integer> s = n;
+		while (list!=null) {
+			n.setNext(list.getNext());
+			list.setNext(list.getNext().getNext());
+			list = list.getNext();
+			n = n.getNext();
+		}
+		
+		return s.getNext();
+		
+	}
+  public static Node<Integer> q1(Node<Integer> a){
+		Node<Integer> st = a;
+		while(a.hasNext()) {
+			while(a.hasNext()) {
+			if(a.getValue()==a.getNext().getValue()) {
+				a.setNext(a.getNext().getNext());
+			}
+			else {
+				a=a.getNext();
+			}
+			}
+			a = st;
+			a = a.getNext();
+		}
+		return st;
+	}
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
